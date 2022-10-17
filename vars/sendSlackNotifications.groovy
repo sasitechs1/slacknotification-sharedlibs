85587ccd9aa3e0ff302913010ed8df8332d29c88
Sasi0328 <112743050+Sasi0328@users.noreply.github.com>
2022-10-17 16:02:44 +0530

    Update sendSlackNotifications.groovy

--- vars/sendSlackNotifications.groovy
+++ vars/sendSlackNotifications.groovy
@@ -1,32 +1,27 @@
-pipeline {
- agent any;
- tools {
-  maven 'maven3.8.5'
-}
+def slackNotifications(String buildStatus = 'STARTED') {
+  // build status of null means successful
+  buildStatus =  buildStatus ?: 'SUCCESS'
+  //buildStatus = buildStatus ? "SUCCESS":"FAILURE"
 
- stages{
- stage ( 'checkout code') {
- steps { 
-   git branch: 'development', credentialsId: 'f2b0ec8f-8deb-4583-bc32-20f466db601d', url: 'https://github.com/sasitechs1/maven-web-application.git'
- } 
-}
+  // Default values
+  def colorName = 'RED'
+  def colorCode = '#FF0000'
+  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+  def summary = "${subject} (${env.BUILD_URL})"
 
- stage ('build') {
- steps{
-  sh "mvn clean package" 
+  // Override default values based on build status
+  if (buildStatus == 'STARTED') {
+    colorName = 'ORANGE'
+    colorCode = '#FFA500'
+  } else if (buildStatus == 'SUCCESS') {
+    colorName = 'GREEN'
+    colorCode = '#00FF00'
+  } else {
+    colorName = 'RED'
+    colorCode = '#FF0000'
   }
-}
-stage ('sonar repo'){
-    steps{
-     sh "mvn sonar:sonar deploy"
-    }
-}
-stage('deploy to tomcat') {
-    steps {
-    sshagent(['980302f0-5430-4688-8f76-1057023e3baa']) {
-    sh "scp -o StrictHostKeyChecking=no target/maven-web-application.war ec2-user@172.31.41.1:/opt/apache-tomcat-9.0.65/webapps/" 
-   }
-   }
-}
- }    
+ 
+
+  // Send notifications
+  slackSend (color: colorCode, message: summary, channel: "#devops")
 }
