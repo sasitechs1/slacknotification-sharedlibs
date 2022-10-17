ed7f6fbe986303d442652f59fb7883c991bc04bb
Sasi0328 <112743050+Sasi0328@users.noreply.github.com>
2022-10-17 15:00:38 +0530

    Update sendSlackNotifications.groovy

--- vars/sendSlackNotifications.groovy
+++ vars/sendSlackNotifications.groovy
@@ -1,1 +1,9 @@
-
+node {
+  def maven : tool name ="maven3.8.5"
+     stage('checkout the code') {
+      git branch: 'development', credentialsId: 'f2b0ec8f-8deb-4583-bc32-20f466db601d', url: 'https://github.com/sasitechs1/maven-web-application.git'
+  }  
+     stage ('build.) {
+      sh "${maven}/bin/mvn clean package"
+      }
+      }
